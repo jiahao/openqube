b614bd16cffe133580e5eaafb1093faaeec6ede8
Jiahao Chen <jiahao@mit.edu>
2012-01-02 15:23:55 -0500

    Rudiments of python package infrastructure

---
+++ setup.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+from setuptools import setup
+setup(name='openqube', author='Jiahao Chen', author_email='jiahao@mit.edu',
+        url='https://github.com/jiahao/openqube', packages=['openqube'])
+
---
+++ src/__init__.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python
+from qchemin import *
+from qchemout import *
+
